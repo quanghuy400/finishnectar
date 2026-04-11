e631a2e66da61ab1556e8e1ca4a1110728e861bf
quanghuy400 <nguyenquang2092005@gmail.com>
2026-04-11 21:29:15 +0700

    fix: hide status bar on splash screen

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,31 +1,23 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, StatusBar } from 'react-native';
 
 export default function SplashScreen({ navigation }) {
 
   useEffect(() => {
     const timer = setTimeout(() => {
       navigation.replace('Onboarding');
-    }, 3000); 
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <View style={styles.container}>
-
-      {/* Background */}
+      <StatusBar hidden={true} />
       <Image
         source={require('../assets/splash Screen.png')}
         style={styles.background}
       />
-
-      {/* Logo chữ */}
-      <Image
-        source={require('../assets/Group 1.png')}
-        style={styles.logo}
-      />
-
     </View>
   );
 }
@@ -33,21 +25,13 @@ export default function SplashScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#4CAF6A',
   },
-
+n  
   background: {
     position: 'absolute',
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
-  },
-
-  logo: {
-    position: 'absolute',
-    alignSelf: 'center',
-    top: '45%',
-    width: 200,
-    height: 80,
-    resizeMode: 'contain',
   },
 });
